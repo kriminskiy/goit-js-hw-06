3d9869b4f0add5602341bef266804944517061d8
kriminskiy <93137873+kriminskiy@users.noreply.github.com>
2022-04-20 10:46:28 +0300

    js-hw6 v2

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,12 @@
-const inputValidation = document.querySelector("#validation-input");
-const numOfCharactersInput = parseInt(inputValidation.dataset.length);
-
-inputValidation.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length > numOfCharactersInput) {
-    inputValidation.classList.add("invalid");
-  } else {
-    inputValidation.classList.add("valid");
+const refs = {
+  formInput: document.querySelector("#validation-input"),
+};
+refs.formInput.addEventListener("blur", (event) => {
+  if (
+    event.currentTarget.value.length > refs.formInput.dataset.length ||
+    event.currentTarget.value.length < refs.formInput.dataset.length
+  ) {
+    return refs.formInput.classList.add("invalid");
   }
+  return refs.formInput.classList.replace("invalid", "valid");
 });
